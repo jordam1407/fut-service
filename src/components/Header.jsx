2e43c1cd86ad4ae5fad50dcb9b534986f62f6091
jordam mendes <jordammendes@gmail.com>
2022-08-25 01:02:25 -0300

    media query, SOFRENDO

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../images/wl-agency-logo.png"
-
+import './header.css'
 export default class Header extends Component {
 
   render() {
