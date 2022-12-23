b656a5e1bf32767f5fcb9479f329833fbef1d4c9
JORDAM MENDES <91645575+jordam1407@users.noreply.github.com>
2022-12-23 01:39:07 -0300

    faqs page chaging texts,some design left

---
+++ src/data/utils.js
@@ -0,0 +1,24 @@
+const steps = [
+  {
+    title: `HOW DOES THE SERVICE WORK?`,
+    text: `Choose a service option on our website, fill out the order form with your account details and schedule preferences, and make your payment. We'll contact you to set up the details and provide a stream account for you to watch the pro-player. The pro-player will start the service at the scheduled time, and you'll receive a notification when it's complete with an image of your finished account. The notification will be attached to an image of your account after the boosting service has been completed, so you can see the results for yourself. This will allow you to confirm that the service has been carried out to your satisfaction and that your goals have been achieved.`
+  },
+  {
+    title: `WHAT IF WE DIDN'T REACH THE RANK ORDERED?`,
+    text: `If you don't reach the rank you ordered, we'll refund the difference in price between the rank you paid for and the rank that was achieved. For example, if you paid for Rank 1 (€70) and our player only achieved Rank 2 (€60), we will refund you the €10 difference. Ensures satisfaction by only charging for achieved rank.`
+  },
+  {
+    title: `I NEED TO SHARE MY ACCOUNT DETAILS?`,
+    text: `In order to use our boosting service, you will need to share your account details with us. This is necessary because the pro-player will need access to your account in order to carry out the boosting service. Your account details will be kept confidential and will only be used for the purpose of providing the service. We take the security of your account seriously and will take all necessary precautions to protect your information.`
+  },
+  {
+    title: `HOW TRUSTABLE THE SERVICE ARE?`,
+    text: `Our boosting service is designed to be as trustworthy as possible. We have a team of highly experienced pro-players who are skilled at providing top-quality boosting services. We also have strict policies in place to ensure the confidentiality and security of your account information. In addition, we offer a full or partial money-back guarantee if somenthing went wrong with your account or if we don't achieve the rank you ordered. We strive to provide the best possible service to our clients and are dedicated to ensuring your satisfaction.`
+  },
+  {
+    title: `I CAN WATCH THE GAMES?`,
+    text: `Yes, you can watch the games while the pro-player is boosting your account. We will provide you with the pro-player's stream account, which you can use to watch the games in real-time. This will allow you to see the progress of the service and get a better understanding of how the pro-player is playing. It can also be a great opportunity to learn and improve your own skills by watching the pro-player in action.`
+  },
+]
+
+export default steps;
--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import logo from "../images/wl-agency-logo.png"
-import '../styles/faqs.css'
-
+import steps from '../data/utils';
 class Faqs extends Component {
   state = {}
 
@@ -9,40 +7,32 @@ class Faqs extends Component {
     const { div, div1, div2, div3, div4 } = this.state;
 
     return (
-      <div className='div-mother-faqs'>
-        <div className='div-son-faqs'>
-          <div className='div-content-space-faqs'>
-            <div>
-              <div className='faqs-answers' onClick={() => this.setState({ div: !div })}>
-                <h2 className='faqs-questions'>HOW DOES THE SERVICE WORK?</h2>
-                {div ? <ul>
-                  <li><span>You buy the chosen service, send the confirmation of the payment to us.</span></li>
-                  <li><span>We'll find the Pro-player to complete the ranking ordered.</span></li>
-                  <li><span>We'll decide together the best moment to do your games.</span></li>
-                </ul> : null}
-              </div>
-              <div className='faqs-answers' onClick={() => this.setState({ div1: !div1 })}>
-                <h2 className='faqs-questions'>WHAT IF YOU DIDN'T REACH THE RANK ORDERED?</h2>
-                {div1 ? <span>If your rank isn't achieved then we'll refund you the difference in pricing between the rank you paid for and the rank that was achieved. So for example if you paid for Rank 1 (€70) and our player achieved Rank 2 (€60) we will refund you €10.</span> : null}
-              </div>
-              <div className='faqs-answers' onClick={() => this.setState({ div2: !div2 })}>
-                <h2 className='faqs-questions'>I NEED TO SHARE MY ACCOUNT DETAILS?</h2>
-                {div2 ? <span>Yes, it's the only way to get access to your WL games. Since the first moment you'll be able to watch on twitch what the player is doing on your account. After games finished you can change your password.</span> : null}
-              </div>
-              <div className='faqs-answers' onClick={() => this.setState({ div3: !div3 })}>
-                <h2 className='faqs-questions'>I CAN WATCH THE GAMES?</h2>
-                {div3 ? <span>After the payment we'll talk to decide the best hour for you and for the player, all games will be on LIVESTREAM, you can watch, comment, share, YOU ARE THE MANAGER!</span> : null}
-              </div>
-              <div className='faqs-answers' onClick={() => this.setState({ div4: !div4 })}>
-                <h2 className='faqs-questions'>HOW TRUSTABLE THE SERVICE ARE?</h2>
-                {div4 ? <span>We have completed more then 150 orders since started on FIFA 22, you can see the testimonials on 'ABOUT' page.</span> : null}
-              </div>
-            </div>
+
+
+
+      <div className="mb-40 lg:my-40 flex flex-col w-11/12 mx-auto">
+          <div className='sm:flex flex-col justify-between' onClick={() => this.setState({ div: !div })}>
+            <h2 className='hover:text-red-700 cursor-pointer border-b border-black-100 xl:text-xl text-xl font-sans pb-2 text-gray-900 font-bold sm:text-5xl sm:leading-none'>{ steps[0].title }</h2>
+            {div ? 
+              <span class='my-9 leading-relaxed text-gray-500 lg:h-20 h-48 text-lg'>{ steps[0].text }</span>
+             : null}
           </div>
-          <div className='content-about'>
-            <img className='logo-faqs' src={logo} alt="CR7" width="500px" />
+          <div className='pt-8 border-t border-black-100 sm:flex flex-col sm:justify-between' onClick={() => this.setState({ div1: !div1 })}>
+            <h2 className='hover:text-red-700 cursor-pointer border-b border-black-100 xl:text-xl text-xl font-sans pb-2 text-gray-900 font-bold sm:text-5xl sm:leading-none'>{ steps[1].title }</h2>
+            {div1 ? <span class='my-9 leading-relaxed text-gray-500 lg:h-20 h-48 text-lg'>{ steps[1].text }</span> : null}
           </div>
-        </div>
+          <div className='pt-8 border-t border-black-100 sm:flex flex-col sm:justify-between' onClick={() => this.setState({ div2: !div2 })}>
+            <h2 className='hover:text-red-700 cursor-pointer border-b border-black-100 xl:text-xl text-xl font-sans pb-2 text-gray-900 font-bold sm:text-5xl sm:leading-none'>{ steps[2].title }</h2>
+            {div2 ? <span class='my-9 leading-relaxed text-gray-500 lg:h-20 h-48 text-lg'>{ steps[2].text }</span> : null}
+          </div>
+          <div className='pt-8 border-t border-black-100 sm:flex flex-col sm:justify-between' onClick={() => this.setState({ div3: !div3 })}>
+            <h2 className='hover:text-red-700 cursor-pointer border-b border-black-100 xl:text-xl text-xl font-sans pb-2 text-gray-900 font-bold sm:text-5xl sm:leading-none'>{ steps[3].title }</h2>
+            {div3 ? <span class='my-9 leading-relaxed text-gray-500 lg:h-20 h-48 text-lg'>{ steps[3].text }</span> : null}
+          </div>
+          <div className='pt-8 border-t border-black-100 sm:flex flex-col sm:justify-between' onClick={() => this.setState({ div4: !div4 })}>
+            <h2 className='hover:text-red-700 cursor-pointer border-b border-black-100 xl:text-xl text-xl font-sans pb-2 text-gray-900 font-bold sm:text-5xl sm:leading-none'>{ steps[4].title }</h2>
+            {div4 ? <span class='my-9 leading-relaxed text-gray-500 lg:h-20 h-48 text-lg'>{ steps[4].text }</span> : null}
+          </div>
       </div>
     );
   }
